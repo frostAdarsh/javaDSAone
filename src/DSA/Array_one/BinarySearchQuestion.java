1a170a4233b3223d908f8562afd9fc5c353833dc
frostAdarsh <adarshmani017@gmail.com>
2025-02-08 19:12:07 +0530

    search cal Occurrence

--- src/DSA/Array_one/BinarySearchQuestion.java
+++ src/DSA/Array_one/BinarySearchQuestion.java
@@ -3,8 +3,18 @@ package DSA.Array_one;
 public class BinarySearchQuestion {
     public static void main(String[] args) {
      int arr[] = {2,2,3,4,4,4,6,8,9};
-     binarySearch(arr,4,true);
-     binarySearch(arr,4,false);
+     int arr2[] = {4,4,8,8,8,15,16,23,23,42};
+//     binarySearch(arr,4,true);
+//     binarySearch(arr,4,false);
+
+        int first = binarySearOccurrence(arr2,15,true);
+        int last = binarySearOccurrence(arr2,15,false);
+        if(first == -1){
+            System.out.println("Occurrence of target is : "+0);
+        } else{
+            System.out.println("Occurrence of target is : "+ (last - first +1));
+
+        }
     }
 
     static void binarySearch(int arr[] ,int target,boolean isFirst){
@@ -32,5 +42,27 @@ public class BinarySearchQuestion {
         else {
             System.out.println("Found element "+ target + " at index : "+ ans);
         }
+    }
+
+    static int binarySearOccurrence(int arr[] ,int target,boolean isFirst){
+        int ans = -1;
+        int start = 0;
+        int end = arr.length-1;
+        while (start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]== target){
+                ans = mid;
+                if(isFirst){
+                    end = mid -1;
+                }else {
+                    start = mid+1;
+                }
+            }else if(arr[mid]<target){
+                start = mid+1;
+            }else {
+                end = mid-1;
+            }
+        }
+        return ans;
     }
 }
