0edf77ffea53089b80866aeef002dd5240624462
frostAdarsh <adarshmani017@gmail.com>
2025-02-09 18:56:18 +0530

    floor and ceil

--- src/DSA/Array_one/BinarySearchQuestion.java
+++ src/DSA/Array_one/BinarySearchQuestion.java
@@ -4,17 +4,24 @@ public class BinarySearchQuestion {
     public static void main(String[] args) {
      int arr[] = {2,2,3,4,4,4,6,8,9};
      int arr2[] = {4,4,8,8,8,15,16,23,23,42,50};
+
 //     binarySearch(arr,4,true);
 //     binarySearch(arr,4,false);
 
-        int first = binarySearOccurrence(arr2,15,true);
-        int last = binarySearOccurrence(arr2,15,false);
-        if(first == -1){
-            System.out.println("Occurrence of target is : "+0);
-        } else{
-            System.out.println("Occurrence of target is : "+ (last - first +1));
+//        int first = binarySearOccurrence(arr2,15,true);
+//        int last = binarySearOccurrence(arr2,15,false);
+//        if(first == -1){
+//            System.out.println("Occurrence of target is : "+0);
+//        } else{
+//            System.out.println("Occurrence of target is : "+ (last - first +1));
+//
+//        }
+        int ans = binarySearchFloor(arr2,17);
+        System.out.println("Ans is : "+ans);
 
-        }
+        int ans1 = binarySearchCeil(arr2,17);
+        System.out.println("Ans is : "+ans1);
+
     }
 
     static void binarySearch(int arr[] ,int target,boolean isFirst){
@@ -61,6 +68,45 @@ public class BinarySearchQuestion {
                 start = mid+1;
             }else {
                 end = mid-1;
+            }
+        }
+        return ans;
+    }
+
+    static int binarySearchFloor(int arr[] ,int target){
+        int ans = -1;
+        int start = 0;
+        int end = arr.length-1;
+        while (start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]== target){
+              ans = arr[mid];
+              break;
+            }else if(arr[mid]<target){
+                start = mid+1;
+                ans = arr[mid];
+            }else {
+                end = mid-1;
+            }
+        }
+       return ans;
+    }
+
+    static int binarySearchCeil(int arr[] ,int target){
+        int ans = -1;
+        int start = 0;
+        int end = arr.length-1;
+        while (start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]== target){
+                ans = arr[mid];
+                break;
+            }else if(arr[mid]<target){
+                start = mid+1;
+
+            }else {
+                end = mid-1;
+                ans = arr[mid];
             }
         }
         return ans;
